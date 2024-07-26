887a4ba4f43e70fad766ce3243528b5b50514690
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-07-26 06:47:23 +0000

    2024-07-26 13:47:23 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/main.go
+++ REST based microservices API development in Golang/banking/main.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
 	"net/http"
 )
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zipcode"`
-}
 
 func main() {
 	//define routes
@@ -20,24 +11,4 @@ func main() {
 
 	//starting server
 	http.ListenAndServe("localhost:8000", nil)
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{Name: "Nam", City: "HCM", Zipcode: "700000"},
-		{Name: "Trang", City: "HCM", Zipcode: "700900"},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
-	}
-
 }
