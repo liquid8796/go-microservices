fcf25ae3beb6b5ac89ad6bf7ec9602e0fcaa5177
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-07-26 06:49:02 +0000

    2024-07-26 13:49:02 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/main.go
+++ REST based microservices API development in Golang/banking/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
 func main() {
-	//define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	//starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	Start()
 }
