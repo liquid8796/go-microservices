eb18b61d94bb3442d3a871455005fe83056d99ba
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-07-26 05:51:11 +0000

    2024-07-26 12:51:11 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/main.go
+++ REST based microservices API development in Golang/banking/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Customer struct {
+	Name    string
+	City    string
+	Zipcode string
+}
 
 func main() {
 	//define routes
@@ -15,4 +22,12 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Nam", City: "HCM", Zipcode: "700000"},
+		{Name: "Trang", City: "HCM", Zipcode: "700900"},
+	}
+	json.NewEncoder(w).Encode(customers)
 }
