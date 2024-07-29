04216b8bde8fc7cec0c50b80c6d5dafb50c7fb79
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-07-29 04:46:54 +0000

    2024-07-29 11:46:54 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/jarvis/main.go
+++ REST based microservices API development in Golang/jarvis/main.go
@@ -23,7 +23,7 @@ func getTime(w http.ResponseWriter, r *http.Request) {
 	tz := r.URL.Query().Get("tz")
 	multiTz := strings.Split(tz, ",")
 
-	fmt.Printf("tz = %s, len = %d", tz, multiTz)
+	fmt.Printf("tz = %s, len = %d", tz, len(multiTz))
 
 	res := map[string]string{}
 	var current_time string
