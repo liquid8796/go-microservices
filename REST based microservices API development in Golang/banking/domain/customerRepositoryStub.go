00e573727d149c2a9fd5f5d4262bcba5d8dcddee
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-01 10:51:25 +0000

    2024-08-01 17:51:25 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/domain/customerRepositoryStub.go
+++ REST based microservices API development in Golang/banking/domain/customerRepositoryStub.go
@@ -11,7 +11,7 @@ func (s CustomerRepositoryStub) FindAll() ([]Customer, error) {
 func NewCustomerRepositoryStub() CustomerRepositoryStub {
 	customers := []Customer{
 		{Id: "1001", Name: "Nam", City: "Thu Dau Mot", Zipcode: "70000", DateOfBirth: "1996-07-08", Status: "1"},
-		{Id: "1002", Name: "Hung", City: "Ho Chi Ming", Zipcode: "100000", DateOfBirth: "1997-01-01", Status: "1"},
+		{Id: "1002", Name: "Hung", City: "Ho Chi Minh", Zipcode: "100000", DateOfBirth: "1997-01-01", Status: "1"},
 	}
 
 	return CustomerRepositoryStub{customers}
