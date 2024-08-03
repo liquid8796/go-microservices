b9d88cf71d68a4d87a643cbcc57e9fe209349910
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-03 04:05:47 +0000

    2024-08-03 11:05:47 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/domain/CustomerRepositoryDb.go
+++ REST based microservices API development in Golang/banking/domain/CustomerRepositoryDb.go
@@ -7,21 +7,14 @@ import (
 )
 
 type CustomerRepositoryDb struct {
+	client *sql.DB
 }
 
 func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
-	client, err := sql.Open("mysql", "root:Qwerty8796/banking")
-	if err != nil {
-		panic(err)
-	}
-
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
 
 	findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
 
-	rows, err := client.Query(findAllSql)
+	rows, err := d.client.Query(findAllSql)
 	if err != nil {
 		log.Println("Error while querying customer table " + err.Error())
 		return nil, err
@@ -39,4 +32,16 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
 		customers = append(customers, c)
 	}
 	return customers, nil
+}
+
+func NewCustomerRepositoryDb() CustomerRepositoryDb {
+	client, err := sql.Open("mysql", "root:Qwerty8796@tcp(localhost:3306)/banking")
+	if err != nil {
+		panic(err)
+	}
+
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return CustomerRepositoryDb{client}
 }
