1bf73a60ac50c83398e801f64b3c8706ae47a945
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-03 06:07:21 +0000

    2024-08-03 13:07:21 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/domain/CustomerRepositoryDb.go
+++ REST based microservices API development in Golang/banking/domain/CustomerRepositoryDb.go
@@ -36,8 +36,17 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
 	return customers, nil
 }
 
-func (d CustomerRepositoryDb) ById(id string) {
+func (d CustomerRepositoryDb) ById(id string) (*Customer, error) {
+	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
 
+	row := d.client.QueryRow(customerSql, id)
+	var c Customer
+	err := row.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
+	if err != nil {
+		log.Println("Error while scanning customer " + err.Error())
+		return nil, err
+	}
+	return &c, nil
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
