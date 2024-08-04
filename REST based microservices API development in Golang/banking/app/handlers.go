4370ccb9ef4fa349a5745f3724cce44448bdd305
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-04 02:29:56 +0000

    2024-08-04 09:29:56 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/app/handlers.go
+++ REST based microservices API development in Golang/banking/app/handlers.go
@@ -48,6 +48,8 @@ func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) 
 		w.WriteHeader(err.Code)
 		// fmt.Fprintf(w, err.Message)
 		json.NewEncoder(w).Encode(err.AsMessage())
+
+		writeResponse(w, err.Code, err.AsMessage())
 	} else {
 		writeResponse(w, http.StatusOK, customer)
 	}
