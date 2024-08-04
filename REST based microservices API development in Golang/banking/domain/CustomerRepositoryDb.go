e789ad44456d8892910f8e1bbb32e8d0d8f39bc9
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-04 02:53:21 +0000

    2024-08-04 09:53:21 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/domain/CustomerRepositoryDb.go
+++ REST based microservices API development in Golang/banking/domain/CustomerRepositoryDb.go
@@ -13,14 +13,13 @@ type CustomerRepositoryDb struct {
 	client *sql.DB
 }
 
-func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
+func (d CustomerRepositoryDb) FindAll() ([]Customer, *errs.AppError) {
 
 	findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
 
 	rows, err := d.client.Query(findAllSql)
 	if err != nil {
-		log.Println("Error while querying customer table " + err.Error())
-		return nil, err
+		return nil, errs.NewNotFoundError("Unexpected database error")
 	}
 
 	customers := make([]Customer, 0)
@@ -30,7 +29,7 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
 
 		if err != nil {
 			log.Println("Error while querying customer table " + err.Error())
-			return nil, err
+			return nil, errs.NewNotFoundError("Unexpected database error")
 		}
 		customers = append(customers, c)
 	}
