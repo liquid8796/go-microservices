90a8a6b846645a1e437a752cfd1360afd707e1e3
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-05 02:42:29 +0000

    2024-08-05 09:42:29 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/service/customerService.go
+++ REST based microservices API development in Golang/banking/service/customerService.go
@@ -14,8 +14,8 @@ type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer() ([]domain.Customer, *errs.AppError) {
-	return s.repo.FindAll()
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *errs.AppError) {
+	return s.repo.FindAll(status)
 }
 
 func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
