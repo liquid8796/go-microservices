734d9dad81b9ac247c4ccbf2f77c1e0705964365
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-06 10:35:38 +0000

    2024-08-06 17:35:38 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/domain/customer.go
+++ REST based microservices API development in Golang/banking/domain/customer.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"banking/dto"
+	"banking/errs"
+)
+
 type Customer struct {
 	Id          string `db:"customer_id"`
 	Name        string
@@ -7,4 +12,20 @@ type Customer struct {
 	Zipcode     string
 	DateOfBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.Status,
+	}
+}
+
+type CustomerRepository interface {
+	FindAll(status string) ([]Customer, *errs.AppError)
+	ById(string) (*Customer, *errs.AppError)
 }
