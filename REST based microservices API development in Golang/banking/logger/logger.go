45a6e7025b3fe32f7ce5c3d7c797bcb99ce7ba93
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-06 04:41:50 +0000

    2024-08-06 11:41:50 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/logger/logger.go
+++ REST based microservices API development in Golang/banking/logger/logger.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var log *zap.Logger
 
 func init() {
 	var err error
+
+	config := zap.NewProductionConfig()
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig = encoderConfig
 
 	log, err = zap.NewProduction(zap.AddCallerSkip(1))
 	if err != nil {
