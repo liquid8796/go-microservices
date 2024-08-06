84b8780c0c693b507846b2f8b246b600cc43dcaa
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-06 10:18:26 +0000

    2024-08-06 17:18:26 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/dto/customerResponse.go
+++ REST based microservices API development in Golang/banking/dto/customerResponse.go
@@ -1,8 +1,8 @@
 package dto
 
 type CustomerResponse struct {
-	Id          string
-	Name        string
+	Id          string `json:"customer_id"`
+	Name        string `json:"full_name"`
 	City        string
 	Zipcode     string
 	DateOfBirth string
