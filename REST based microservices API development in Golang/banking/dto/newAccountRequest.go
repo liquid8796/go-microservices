2fab016d4f1445b48aa5ca9f8a2d7070a36aa706
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-09 14:31:05 +0000

    2024-08-09 21:31:05 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/dto/newAccountRequest.go
+++ REST based microservices API development in Golang/banking/dto/newAccountRequest.go
@@ -1,6 +1,9 @@
 package dto
 
-import "banking/errs"
+import (
+	"banking/errs"
+	"strings"
+)
 
 type NewAccountRequest struct {
 	CustomerId  string  `json:"customer_id"`
@@ -12,7 +15,7 @@ func (r NewAccountRequest) Validate() *errs.AppError {
 	if r.Amount < 5000 {
 		return errs.NewValidationError("To open a new account you need to deposit atleast 5000.00")
 	}
-	if r.AccountType != "saving" && r.AccountType != "checking" {
+	if strings.ToLower(r.AccountType) != "saving" && r.AccountType != "checking" {
 		return errs.NewValidationError("Account type should be checking or saving")
 	}
 	return nil
