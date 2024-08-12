a5e8934dc4642eccd2f4e9891a362a65d94224a4
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-12 11:11:05 +0000

    2024-08-12 18:11:05 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/dto/transaction_test.go
+++ REST based microservices API development in Golang/banking/dto/transaction_test.go
@@ -4,8 +4,14 @@ import "testing"
 
 func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal(t *testing.T) {
 	// Arrange
-
+	request := TransactionRequest{
+		TransactionType: "invalid transaction type",
+	}
 	// Act
+	appError := request.Validate()
 
 	// Assert
+	if appError.Message != "Transaction type can only be deposit or withdrawal" {
+
+	}
 }
