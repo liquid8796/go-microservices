ebc1a1ad40c7e67dd8b428867cf6ee9500c142b3
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-12 12:33:13 +0000

    2024-08-12 19:33:13 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/dto/transaction_test.go
+++ REST based microservices API development in Golang/banking/dto/transaction_test.go
@@ -10,6 +10,7 @@ func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal
 	request := TransactionRequest{
 		TransactionType: "invalid transaction type",
 	}
+
 	// Act
 	appError := request.Validate()
 
@@ -21,4 +22,12 @@ func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal
 	if appError.Code != http.StatusUnprocessableEntity {
 		t.Error("Invalid code while testing transaction type")
 	}
+}
+
+func Test_should_return_error_when_amount_is_less_than_zero(t *testing.T) {
+	// Arrange
+	request := TransactionRequest{TransactionType: DEPOSIT, Amount: -100}
+
+	// Act
+	appError := request.Validate()
 }
