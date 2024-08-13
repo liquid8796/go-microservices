83232a1295d3961640a65c69cce9b5457098af67
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-08-13 16:56:53 +0000

    2024-08-13 23:56:53 +07:00 Asia/Saigon

--- REST based microservices API development in Golang/banking/app/customerHandler_test.go
+++ REST based microservices API development in Golang/banking/app/customerHandler_test.go
@@ -16,13 +16,17 @@ var router *mux.Router
 var ch CustomerHandlers
 var mockService *service.MockCustomerService
 
-func setup(t *testing.T) {
+func setup(t *testing.T) func() {
 	ctrl := gomock.NewController(t)
 	mockService := service.NewMockCustomerService(ctrl)
 	ch = CustomerHandlers{mockService}
 
 	router = mux.NewRouter()
 	router.HandleFunc("/customers", ch.getAllCustomers)
+
+	return func() {
+		router = nil
+	}
 }
 
 func Test_should_return_customers_with_status_code_200(t *testing.T) {
