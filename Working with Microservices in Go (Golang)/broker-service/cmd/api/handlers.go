ce452cf70990e8d67a6ac88c1cad4e129683f24a
liquid8796 <hanam.tranle.5@gmail.com>
2024-08-29 10:07:58 +0000

    2024-08-29 17:07:58 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/broker-service/cmd/api/handlers.go
+++ Working with Microservices in Go (Golang)/broker-service/cmd/api/handlers.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 )
 
@@ -47,8 +46,6 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	// create some json we'll send to the auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
-
-	fmt.Println(">>>>>>>>>>>Authenticating")
 
 	// call the service
 	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
