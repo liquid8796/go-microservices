c8ec1220258b3197b99bb33924a79389290a008e
liquid8796 <hanam.tranle.5@gmail.com>
2024-08-30 13:51:00 +0000

    2024-08-30 20:51:00 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/logger-service/cmd/api/main.go
+++ Working with Microservices in Go (Golang)/logger-service/cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,9 +22,23 @@ type Config struct {
 
 func main() {
 	// connect to mongo
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
 	clientOptions := options.Client().ApplyURI(mongoURL)
 	clientOptions.SetAuth(options.Credential{
 		Username: "admin",
 		Password: "password",
 	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
 }
