6b5d5c96c443da1826e772ac2bc59669c82301dc
liquid8796 <hanam.tranle.5@gmail.com>
2024-08-30 13:33:18 +0000

    2024-08-30 20:33:18 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/logger-service/cmd/api/main.go
+++ Working with Microservices in Go (Golang)/logger-service/cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
+import "go.mongodb.org/mongo-driver/mongo"
+
 const (
 	webPort  = "80"
 	rpcPort  = "5001"
 	mongoURL = "mongodb://mongo:27017"
 	gRpcPort = "50001"
 )
+
+var client *mongo.Client
 
 func main() {
 
