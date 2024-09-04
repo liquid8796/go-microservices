3cfe82ee8f068040d1600fcca5c9bf988f510294
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-04 11:45:53 +0000

    2024-09-04 18:45:53 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/broker-service/cmd/api/handlers.go
+++ Working with Microservices in Go (Golang)/broker-service/cmd/api/handlers.go
@@ -110,7 +110,7 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		app.errorJSON(w, errors.New("invalid credentials"))
 		return
 	} else if response.StatusCode != http.StatusAccepted {
-		app.errorJSON(w, errors.New(fmt.Sprintf("%s - error calling auth service", response.Body)))
+		app.errorJSON(w, errors.New(fmt.Sprintf("%s - error calling auth service", json.NewDecoder(response.Body))))
 		return
 	}
 
