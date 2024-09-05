6e617bab0203a721c723b1fa501ff3f18ade290d
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-05 16:12:40 +0000

    2024-09-05 23:12:40 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/authentication-service/cmd/api/helpers.go
+++ Working with Microservices in Go (Golang)/authentication-service/cmd/api/helpers.go
@@ -38,11 +38,11 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 		return err
 	}
 
-	// if len(headers) > 0 {
-	// 	for key, value := range headers[0] {
-	// 		w.Header()[key] = value
-	// 	}
-	// }
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
