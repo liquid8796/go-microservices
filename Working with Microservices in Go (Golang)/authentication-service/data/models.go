9a87dfd35b2b7e3a8eb8f9bb2ef8d2bbf76b69cf
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-05 10:50:46 +0000

    2024-09-05 17:50:46 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/authentication-service/data/models.go
+++ Working with Microservices in Go (Golang)/authentication-service/data/models.go
@@ -88,7 +88,7 @@ func (u *User) GetByEmail(email string) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from public.users where email = $1`
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users where email = $1`
 
 	var user User
 	row := db.QueryRowContext(ctx, query, email)
