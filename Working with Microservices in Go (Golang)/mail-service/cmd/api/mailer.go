6a4c355ce6ca6b9491c4c4f868368f6a08ea4e5a
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-13 06:04:20 +0000

    2024-09-13 13:04:20 +07:00 Asia/Bangkok

---
+++ Working with Microservices in Go (Golang)/mail-service/cmd/api/mailer.go
@@ -0,0 +1,12 @@
+package main
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+}
