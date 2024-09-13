78f0d6842a1b7332fcef64e9b0eda9acedbf5c0a
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-13 09:49:12 +0000

    2024-09-13 16:49:12 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/broker-service/cmd/api/handlers.go
+++ Working with Microservices in Go (Golang)/broker-service/cmd/api/handlers.go
@@ -160,4 +160,11 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 
 	request.Header.Set("Content-Type", "application/json")
 
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
 }
