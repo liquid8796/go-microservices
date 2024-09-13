a5791e87aa70cebac906ccd530789ef40433f4de
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-13 08:57:55 +0000

    2024-09-13 15:57:55 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/mail-service/cmd/api/main.go
+++ Working with Microservices in Go (Golang)/mail-service/cmd/api/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
@@ -26,4 +28,20 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return m
 }
