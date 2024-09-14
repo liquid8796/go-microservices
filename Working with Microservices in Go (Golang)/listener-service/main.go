376ce40c1c8e76cb70342f168f02816d620db50a
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-14 04:56:57 +0000

    2024-09-14 11:56:57 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/listener-service/main.go
+++ Working with Microservices in Go (Golang)/listener-service/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	_ "github.com/rabbitmq/amqp091-go"
+	"fmt"
+	"math"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -12,4 +16,34 @@ func main() {
 	// create consumer
 
 	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		fmt.Println("Backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
