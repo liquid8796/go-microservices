213f2aad5741b2c0b1d586a3b11af4fd339e4965
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-16 17:06:37 +0000

    2024-09-17 00:06:37 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/broker-service/event/emitter.go
+++ Working with Microservices in Go (Golang)/broker-service/event/emitter.go
@@ -1,6 +1,8 @@
 package event
 
 import (
+	"log"
+
 	_ "github.com/rabbitmq/amqp091-go"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -17,4 +19,30 @@ func (e *Emitter) setup() error {
 
 	defer channel.Close()
 	return declareExchange(channel)
+}
+
+func (e *Emitter) Push(event string, severity string) error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+
+	log.Println("Pushing to channel")
+
+	err = channel.Publish(
+		"log_topic",
+		severity,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(event),
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
