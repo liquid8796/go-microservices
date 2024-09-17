05623d5a6bc4ad50e027f14345551ea2c4294bc6
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-17 06:47:45 +0000

    2024-09-17 13:47:45 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/logger-service/cmd/api/main.go
+++ Working with Microservices in Go (Golang)/logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -66,8 +68,22 @@ func (app *Config) serve() {
 	}
 }
 
-func (app *Config) rpcListten() {
+func (app *Config) rpcListten() error {
 	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+
 }
 
 func connectToMongo() (*mongo.Client, error) {
