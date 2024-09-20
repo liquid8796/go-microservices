5188dfc239ade0d9e0a0d47494b1d0dbfbca95c4
liquid8796 <hanam.tranle.5@gmail.com>
2024-09-20 06:02:04 +0000

    2024-09-20 13:02:04 +07:00 Asia/Bangkok

--- Working with Microservices in Go (Golang)/broker-service/cmd/api/routes.go
+++ Working with Microservices in Go (Golang)/broker-service/cmd/api/routes.go
@@ -11,22 +11,29 @@ import (
 func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
-	// specify who is allowed to connect
+	// specify who is allowed to connect to our API service
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://*", "https://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTION"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
 
+	// a heartbeat route, to ensure things are up
 	mux.Use(middleware.Heartbeat("/ping"))
+
+	// this route is just to ensure things work, and is never
+	// used after that
+	mux.Get("/", app.Broker)
 
 	mux.Post("/", app.Broker)
 
+	// grpc route
 	mux.Post("/log-grpc", app.LogViaGRPC)
 
+	// a route for everything
 	mux.Post("/handle", app.HandleSubmission)
 
 	return mux
